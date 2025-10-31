98119af916220f41e968cb55acf275726d4a8759
pc asus <jrsilva.lorivas@gmail.com>
2025-10-30 23:16:12 -0300

    first commit

---
+++ src/main/java/br/com/lorijr/conta_zen_api/ContaZenApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.lorijr.conta_zen_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaZenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaZenApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lorijr/conta_zen_api/ContaZenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lorijr.conta_zen_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaZenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
