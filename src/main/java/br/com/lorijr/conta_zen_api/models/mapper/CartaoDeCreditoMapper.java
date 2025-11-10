a3d39a1b3968aeb994da062815c6822bb4516ac3
pc asus <jrsilva.lorivas@gmail.com>
2025-11-10 19:33:25 -0300

    fix import class

--- src/main/java/br/com/lorijr/conta_zen_api/models/mapper/CartaoDeCreditoMapper.java
+++ src/main/java/br/com/lorijr/conta_zen_api/models/mapper/CartaoDeCreditoMapper.java
@@ -1,6 +1,7 @@
 package br.com.lorijr.conta_zen_api.models.mapper;
 
 import br.com.lorijr.conta_zen_api.models.CartaoDeCredito;
+import br.com.lorijr.conta_zen_api.models.dto.CartaoDeCreditoDTO;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
--- src/main/java/br/com/lorijr/conta_zen_api/models/mapper/FaturaMapper.java
+++ src/main/java/br/com/lorijr/conta_zen_api/models/mapper/FaturaMapper.java
@@ -3,6 +3,7 @@ package br.com.lorijr.conta_zen_api.models.mapper;
 import br.com.lorijr.conta_zen_api.models.Fatura;
 import br.com.lorijr.conta_zen_api.models.dto.FaturaDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 import java.util.List;
 
@@ -12,5 +13,5 @@ public interface FaturaMapper {
     FaturaDTO toDTO(Fatura fatura);
     List<FaturaDTO> toDTO(List<Fatura> faturas);
     Fatura toEntity(FaturaDTO faturaDTO);
-    void updateFromDTO(FaturaDTO faturaDTO, Fatura fatura);
+    void updateFromDTO(FaturaDTO faturaDTO, @MappingTarget Fatura fatura);
     }
