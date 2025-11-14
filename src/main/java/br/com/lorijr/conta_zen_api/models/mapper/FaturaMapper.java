9e3a10aac60108068d15d2fdffe131a8041366cf
pc asus <jrsilva.lorivas@gmail.com>
2025-11-14 00:24:14 -0300

    refact mapping to ContainerContaAvulsaDTO

--- src/main/java/br/com/lorijr/conta_zen_api/models/mapper/FaturaMapper.java
+++ src/main/java/br/com/lorijr/conta_zen_api/models/mapper/FaturaMapper.java
@@ -1,18 +1,54 @@
 package br.com.lorijr.conta_zen_api.models.mapper;
 
 import br.com.lorijr.conta_zen_api.models.Fatura;
+import br.com.lorijr.conta_zen_api.models.ItemConta;
+import br.com.lorijr.conta_zen_api.models.dto.ContainerContaAvulsaDTO;
 import br.com.lorijr.conta_zen_api.models.dto.FaturaDTO;
+import br.com.lorijr.conta_zen_api.models.dto.ItemContaDTO;
+import lombok.RequiredArgsConstructor;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.factory.Mappers;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Collections;
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {ItemContaMapper.class})
 public interface FaturaMapper {
 
     @Mapping(source = "valorTotal", target = "valorTotal")
+    @Mapping(target = "contaAvulsa", expression = "java(mapContaAvulsa(fatura))")
     FaturaDTO toDTO(Fatura fatura);
+
+    default ContainerContaAvulsaDTO mapContaAvulsa(Fatura fatura){
+        if(fatura == null) return null;
+
+        ItemContaMapper itemContaMapper = Mappers.getMapper(ItemContaMapper.class);
+        ContainerContaAvulsaDTO container = new ContainerContaAvulsaDTO();
+        container.setListaItens(itemContaMapper.toItemContaDTO(fatura.getContaAvulsa()));
+
+        container.setTotalItensAvulsos(fatura.getTotalContasAvulsas());
+
+        return container;
+    }
+
+    // Adicione este método ao FaturaMapper.java
+    default List<ItemConta> map(ContainerContaAvulsaDTO container) {
+        if (container == null) return null;
+
+        // 1. Pega a lista de DTOs do Container
+        List<ItemContaDTO> itensDto = container.getListaItens();
+
+        // 2. Converte a lista de DTOs de volta para Entidades (ItemConta)
+        // Usando o mapper auxiliar (acessado via Mappers.getMapper())
+        ItemContaMapper itemContaMapper = Mappers.getMapper(ItemContaMapper.class);
+
+        return itemContaMapper.toItemConta(itensDto);
+        // OBS: O nome do método no ItemContaMapper deve ser 'toItemConta' e aceitar List<ItemContaDTO>
+    }
+
     List<FaturaDTO> toDTO(List<Fatura> faturas);
     Fatura toEntity(FaturaDTO faturaDTO);
     void updateFromDTO(FaturaDTO faturaDTO, @MappingTarget Fatura fatura);
--- src/main/java/br/com/lorijr/conta_zen_api/models/mapper/ItemContaMapper.java
+++ src/main/java/br/com/lorijr/conta_zen_api/models/mapper/ItemContaMapper.java
@@ -4,8 +4,10 @@ import br.com.lorijr.conta_zen_api.models.ItemConta;
 import br.com.lorijr.conta_zen_api.models.dto.ItemContaDTO;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface ItemContaMapper {
-    ItemContaDTO toItemContaDTO(ItemConta itemConta);
-    ItemConta toItemConta(ItemContaDTO itemContaDTO);
+    List<ItemContaDTO> toItemContaDTO(List<ItemConta> itemConta);
+    List<ItemConta> toItemConta(List<ItemContaDTO> itemContaDTO);
 }
